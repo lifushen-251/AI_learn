f3b8d2176c02fb9711d0cd18ad9d01a43d14497c
Your Name <your-email@example.com>
2025-11-14 19:37:54 +0800

    Add new note

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-# # 创建空集合
-# Set=set()    #定义空集合
-# print(type(Set))
+# 创建空集合
+Set=set()    #定义空集合
+print(type(Set))
 
-# #添加一个元素
-# Set.add("114")
-# print(Set)
+#添加一个元素
+Set.add("114")
+print(Set)
 
-# #将一个可迭代对象依次加到集合中
-# Set.update([1, '1', '4'])
-# print(Set)
-import matplotlib.pyplot as plt
+#将一个可迭代对象依次加到集合中
+Set.update([1, '1', '4'])
+print(Set)
